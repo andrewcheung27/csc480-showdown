3abb5acae0f245db85690a33e18efb59b6a1c09e
Andrew Cheung <andrewcheung27@gmail.com>
2024-05-18 09:34:41 -0700

    Initial commit for csc 480 project

---
+++ showdown/battle_bots/search/main.py
@@ -0,0 +1,16 @@
+from showdown.battle import Battle
+
+from ..helpers import format_decision
+from ..helpers import pick_safest_move_from_battles
+
+
+class BattleBot(Battle):
+    def __init__(self, *args, **kwargs):
+        super(BattleBot, self).__init__(*args, **kwargs)
+
+    def find_best_move(self):
+        # TODO: implement search-based bot by replacing this function.
+        #  see showdown/battle_bots/helpers.py for helper functions.
+        battles = self.prepare_battles(join_moves_together=True)
+        safest_move = pick_safest_move_from_battles(battles)
+        return format_decision(self, safest_move)
