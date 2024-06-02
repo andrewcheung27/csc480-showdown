4507321d9cf2b0427dfa64556aa5696e0506a63e
Andrew Cheung <andrewcheung27@gmail.com>
2024-06-02 09:58:55 -0700

    fixed indents

--- bot-demo.sh
+++ bot-demo.sh
@@ -7,11 +7,11 @@
 # run the loop inside a subshell with SIGINT killing so that all bot matches can be killed with ctrl-c
 # (see https://stackoverflow.com/a/52033580)
 (trap 'kill 0' SIGINT;
-battle_bot=most_damage
+  battle_bot=most_damage
   # search bot vs. battle bot: battles with the same team
-for i in {1..1}; do
-  python3 bot_match.py -e env_specificteam -t gen5/gyara-1v1 --log-record logs/record-gyara-1v1-vs-$battle_bot.log &
-  python3 bot_match.py -e env_specificteam_$battle_bot -t gen5/gyara-1v1 &
-  wait
-done
+  for i in {1..1}; do
+    python3 bot_match.py -e env_specificteam -t gen5/gyara-1v1 --log-record logs/record-gyara-1v1-vs-$battle_bot.log &
+    python3 bot_match.py -e env_specificteam_$battle_bot -t gen5/gyara-1v1 &
+    wait
+  done
 )
